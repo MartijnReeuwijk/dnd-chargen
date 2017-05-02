0f0598d39ff603186e139630fd82c95ec406ff1d
Martijn <m.reeuwijk@live.nl>
2017-05-03 00:18:53 +0200

    link
    links

---
+++ js/path.js
@@ -0,0 +1,6 @@
+document.querySelector(".Player").onclick = function () {
+    location.href = "pages/player.html";
+};
+document.querySelector(".Dm").onclick = function () {
+    location.href = "pages/dm.html";
+};
