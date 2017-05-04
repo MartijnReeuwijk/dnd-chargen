86655f1d50f711c78000eb39944e7fa31803d9f6
Martijn <m.reeuwijk@Live.nl>
2017-05-04 16:27:16 +0200

    Changed all imperial to metric function
    Did my to do and changed all the old functions the the new function mode
    will change other function to this

--- js/dm.js
+++ js/dm.js
@@ -9,63 +9,73 @@
 	    euUnits.classList.toggle('active2'); // or whatever your active class is
 	});
 
+function giveMeMeters() {
+	var feets = document.querySelector(".feetInput").value;
+	var meters = feetToMeter(feets);
+	document.querySelector(".feet p").innerHTML = "<p>" + meters + "M" + "</p>";
+
+}
 
 // feet to meter calc
 function feetToMeter() {
-	var feet = document.querySelector(".feetInput").value;
-	var floormeter = feet * 0.3048;
-	var meter = floormeter.toFixed(2);
-	console.log(meter);
-	document.querySelector(".feet p").innerHTML = "<p>" + meter + "M" + "</p>";
+	var floormeters = feets * 0.3048;
+	var meters = floormeter.toFixed(2);
+return meters
 }
 
-function whatTheFuckIsaPint() {
-	var pint = document.querySelector(".pintInput").value;
-		// look at this wtf murica
-	var floorliter = pint * 0.473176473;
-	var liters = floorliter.toFixed(2);
+function giveMePints(){ // please i can use one
+	var pints = document.querySelector(".pintInput").value;
+	var liters = whatTheFuckIsaPint(pints);
 	document.querySelector(".pint p").innerHTML = "<p>" + liters + "L" +"</p>";
-	console.log(liters);
+
+}
+function whatTheFuckIsaPint() {
+	var floorliters = pints * 0.473176473;
+	var liters = floorliter.toFixed(2);
+	return liters;
 }
 
-// since i dont return the value i can use the same varable name
-//event listener
-
-// TODO: change all function to this format
 function giveMeGallons(){
 	var gallons = document.querySelector(".gallonInput").value;
 	var liters = gallonToLiter(gallons);
 	document.querySelector(".gallon p").innerHTML = "<p>" + liters + "L" +"</p>";
 }
 function gallonToLiter(gallons) {
-	var floorliter = gallons * 3.78541178;
+	var floorliters = gallons * 3.78541178;
 	var liters = floorliter.toFixed(2);
 	return liters;
 }
 
-// console.log(gallonToLiter(16));
-// var userGallons = document.querySelector(".gallonInput").value;
-// document.querySelector(".gallon p").innerHTML = "<p>" + gallonToLiter(16) + "L" +"</p>";
-
-function ounceToGram() {
-	var ounce = document.querySelector(".ounceInput").value;
-// cool measurements GB ounce is not a eu ounce
-	var floorOunce = ounce * 28.3495231;
-	var gram = floorOunce.toFixed(2);
-	document.querySelector(".ounce p").innerHTML = "<p>" + gram + "GR" + "</p>";
+// My understanding make a function to get the variables then use var grams why
+// triggers the ounceToGram which the input value from var ounce which return the calculated ounceToGram
+// which is the amount of grams i need to place in the html
+// now i have dissected i understand why this is better you can still use the ounceToGram annywhere
+function giveMeGrams(){
+	var ounces = document.querySelector(".ounceInput").value;
+	var grams = ounceToGram(ounces)
+	document.querySelector(".ounce p").innerHTML = "<p>" + grams + "GR" + "</p>";
 }
 
+function ounceToGram(ounces) {
+	var floorOunces = ounces * 28.3495231;
+	var grams = floorOunce.toFixed(2);
+	return grams;
+}
+function giveMeKg(){
+	var lbs = document.querySelector(".lbInput").value;
+	var kgs = lbToKg(lbs);
+	document.querySelector(".lb p").innerHTML = "<p>" + kgs + "KG" + "</p>";
+
+}
 function lbToKg() {
-	var lb = document.querySelector(".lbInput").value;
-	var floorLb = lb * 0.45359237;
-	var kg = floorLb.toFixed(2);
-	console.log(kg);
-	document.querySelector(".lb p").innerHTML = "<p>" + kg + "KG" + "</p>";
+	var floorLbs = lbs * 0.45359237;
+	var kgs = floorLb.toFixed(2);
+	return kgs;
 }
 
 
-document.querySelector(".feet").addEventListener("change", feetToMeter);
-document.querySelector(".pint").addEventListener("change", whatTheFuckIsaPint);
+document.querySelector(".feet").addEventListener("change", giveMeMeters);
+document.querySelector(".pint").addEventListener("change", giveMePints);
 document.querySelector(".gallon").addEventListener("change", giveMeGallons);
-document.querySelector(".ounce").addEventListener("change", ounceToGram);
-document.querySelector(".lb").addEventListener("change", lbToKg);
+document.querySelector(".ounce").addEventListener("change", giveMeGrams);
+document.querySelector(".lb").addEventListener("change", giveMeKg);
