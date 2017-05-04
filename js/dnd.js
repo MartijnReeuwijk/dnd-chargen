9bda38cdc9ed0e4721a42acd26720ff36a37489b
Martijn <m.reeuwijk@Live.nl>
2017-05-04 16:36:42 +0200

    removed old stuff

--- js/dnd.js
+++ js/dnd.js
@@ -38,9 +38,6 @@ function characterValue(){
   var roleToString = roleSelect.options[roleSelect.selectedIndex].value;
   var role = roleToString.toString();
 
-  // TODO: add event so when you update your whatever the function fires and puts it in the Object
-  // so many vars jezus
-
   character.age = age;
   character.alignment = alignment;
   character.race = race;
@@ -61,30 +58,6 @@ function roll(){
   var lowestValue =  Math.min.apply( Math, allrolls );
   var abilityScores = resultroll - lowestValue ;
   var modifier = 0;
-
-  // calc the modifiers here
-
-  // this doesnt work as i hoped need it to this only works in a switch case
-  //{
-  //   var allrolls = [];
-  //
-  //   console.log(allrolls);
-  //   // var resultroll = roll + roll1 + roll2 + roll3;
-  //   // var lowestValue =  Math.min.apply( Math, allrolls );
-  //   // var abilityScores = resultroll - lowestValue ;
-  //   console.log(abilityScores);
-  //
-  // // rolls like this
-  //   for (var i = 1; i <= 4; i++) {
-  //
-  //     roll[i] = d6 = Math.floor((Math.random() * 6)+1);
-  //     allrolls.push(roll[i]);
-  //     // this works found it online how the fuck could i have known.
-  //     var abilityScores = allrolls.reduce(function (a, b) {
-  //       return a + b;
-  //     });
-  //   }
-  //}
 
   if (abilityScores >= 3) {
     modifier = -4;
@@ -113,22 +86,13 @@ function roll(){
   if (abilityScores <=19 && abilityScores >17) {
     modifier = +4;
   }
-  // else {
-  // 	alert("Leed hacks dit kan je niet rollen met 4x6 - de laagste")
-  // }
-  // var name = exampleElement.name;
-
-  // i want the selected element and add the name of the selected element so i know if its strength / dexterity
-
-  // this can be done with the THIs element but dont  know how
   document.querySelector(".strength").innerHTML = "<p>" + abilityScores + "</p>" + "<p>" + modifier + "</p>";
   character.abilityScores = abilityScores;
   character.modifier = modifier;
 };
+
 function calcArmorclass(){
-  // cent get role fron global idk why
   var role = character.role;
-  // case dont work
   switch (role) {
     case "Barbarian":
     var armorclass = 10 + dexterityMod + constitutionMod;
@@ -145,12 +109,10 @@ function playerValue(){
 
   character.playerName = name;
   character.playerLastName = lastName;
-
 };
-function calcHitpoints(role){
-  var role = character.role;
 
-  // get that levelCalc magic
+function calcHitpoints(){
+  var role = character.role; // get role from char Object
   var level = levelCalc(level);
   switch (role) {
     case "Wizard" || "Cleric" || "Sorcerer":
@@ -164,12 +126,10 @@ function calcHitpoints(role){
         return a + b;
       });
     }
-
     break;
 
     case "Rogue" || "Warlock" || "Bard":
     var allrolls = [];
-
     for (var i = 1; i <= level; i++) {
       roll[i] = d8 = Math.floor((Math.random() * 8)+1) + constitutionMod;
       allrolls.push(roll[i]);
@@ -178,12 +138,10 @@ function calcHitpoints(role){
         return a + b;
       });
     }
-
     break;
 
     case "Paladin" || "Fighter" || "Ranger":
     var allrolls = [];
-
     for (var i = 1; i <= level; i++) {
       roll[i] = d10 = Math.floor((Math.random() * 10)+1); + constitutionMod;
       allrolls.push(roll[i]);
@@ -192,13 +150,10 @@ function calcHitpoints(role){
         return a + b;
       });
     }
-
     break;
 
-    // default is the Barbarian im so smart
     default:
     var allrolls = [];
-
     for (var i = 1; i <= level; i++) {
       roll[i] = d12 = Math.floor((Math.random() * 12)+1) + constitutionMod;
       allrolls.push(roll[i]);
@@ -207,26 +162,19 @@ function calcHitpoints(role){
         return a + b;
       });
     }
-
     character.hitpoints = hitpoints;
-
   }
-
   document.querySelector(".hitpoints .center").innerHTML = "<h2>" + "hitpoints" + "</h2>" + "<p>" + hitpoints + "</p>";
   character.hitpoints = hitpoints;
-
-  // always return last
   return hitpoints;
 
 };
 function levelCalc(level){
-  // one spelling error 30m of crying :"(   getElementsById -->  getElementById
   var xp = document.getElementById("xp").value;
 
   if (xp < 300) {
     level = 1;
   }
-  // forgot the > RIP life
   if (xp >= 300 && xp < 900) {
     level = 2;
   }
@@ -269,39 +217,30 @@ function levelCalc(level){
 };
 // TODO: some magic so the bonusses are acually connected
 function placeMods(){
-
-
-
   var strModPlace = document.querySelectorAll(".str p");
   for (var i = 0; i < strModPlace.length; i++) {
     strModPlace[i].innerHTML = "<p>" + strengthMod + "</p>";
   }
-
   var dexModPlace = document.querySelectorAll(".dex p");
   for (var i = 0; i < dexModPlace.length; i++) {
     dexModPlace[i].innerHTML = "<p>" + dexterityMod + "</p>";
   }
-
   var conModPlace = document.querySelectorAll(".con p");
   for (var i = 0; i < conModPlace.length; i++) {
     conModPlace[i].innerHTML = "<p>" + constitutionMod + "</p>";
   }
-
   var intModPlace = document.querySelectorAll(".int p");
   for (var i = 0; i < intModPlace.length; i++) {
     intModPlace[i].innerHTML = "<p>" + intelligenceMod + "</p>";
   }
-
   var wisModPlace = document.querySelectorAll(".wis p");
   for (var i = 0; i < wisModPlace.length; i++) {
     wisModPlace[i].innerHTML = "<p>" + wisdomMod + "</p>";
   }
-
   var charModPlace = document.querySelectorAll(".cha p");
   for (var i = 0; i < charModPlace.length; i++) {
     charModPlace[i].innerHTML = "<p>" + charismaMod + "</p>";
   }
-
 }
 
 // now + proficiencyBonus on the selected checkbox
